88fcddc7cfb7f0ac7a39a89c5e34da72df8e854c
guydk <info@guydekort.nl>
2025-04-21 11:46:44 +0200

    Delete layers directory

--- layers/SPORTbpSportparkDenDonk5maxbebouwd_3.js
+++
@@ -1,1 +0,0 @@
-var json_SPORTbpSportparkDenDonk5maxbebouwd_3 = {"type":"FeatureCollection","name":"SPORTbpSportparkDenDonk5maxbebouwd_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
